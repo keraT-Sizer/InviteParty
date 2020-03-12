6518363bf93ea43d907d2cc8b05d152affdcc4be
Tarek Aouadhi <tarekaouadhi@gmail.com>
2020-03-12 15:49:19 +0100

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestRespnse guestRespnse) {
+            // TODO: store response from guest
+            if (ModelState.IsValid) { 
+            Repository.AddResponse(guestRespnse);
+            return View("Thanks", guestRespnse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+        //public IActionResult Index() {
+        //    return View();
+        //}
+
+        //public IActionResult About() {
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact() {
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Privacy() {
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error() {
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+        //public string Index() {
+        //    return "Hello World";
+        //}
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models {
+    public class Repository {
+        private static List<GuestRespnse> responses = new List<GuestRespnse>();
+        public static IEnumerable<GuestRespnse> Responses {
+            get {
+                return responses;
+            }
+        }
+        public static void AddResponse(GuestRespnse response) {
+            responses.Add(response);
+        }
+
+        }
+    }
+
